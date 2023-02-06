165b0dc5b13ebb1a5299ef2ad8ea822b0975e47f
Vusal <vusal.feyruz@gmail.com>
2023-02-06 20:35:02 +0400

    Proqram tam isghlek veziyyete getirildi, elvae ozellikler yaradildi

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const sortAll = document.getElementById("all");
 const sortPend = document.getElementById("pending");
 const sortComp = document.getElementById("completed");
 
+const deleteAll = document.querySelector(".deleteAll");
+
 
 // allTask object local stroage yoxlayir eger tasklist varsa onu menimseyir yoxdursa bosh array menimseyir
 
 let allTasks = localStorage.getItem("taskList") === null ? [] : JSON.parse(localStorage.getItem("taskList"));
-
-let taskList; 
 
 const sortStatusType = (sortType) => {
     if(sortType === "pending" || sortType === "completed"){
@@ -22,15 +22,20 @@ const sortStatusType = (sortType) => {
 
 sortsType.forEach(item => {
     item.addEventListener("click", () => {
+
         sortsType.forEach(itemclass => {
             itemclass.classList.contains("selected-sort") && itemclass.classList.remove("selected-sort")
         })
         item.classList.add("selected-sort")
+
         sortStatusType(item.getAttribute("status"));
+
         displayTasks()
         allTasks = JSON.parse(localStorage.getItem("taskList"))
     })
 }) 
+
+
 const updateTaskStatus = (id, element) => {
     allTasks.forEach(task => {
         if(task.id === id) {
@@ -44,35 +49,48 @@ const updateTaskStatus = (id, element) => {
                 }
         }
     })
-    
+
     displayTasks()
-    localStorage.setItem("taskList", JSON.stringify(allTasks))
+    localStorage.setItem("taskList", JSON.stringify(allTasks));
 }
 
 // allTasks icindeki elementleri (tasklari) htmle gonderir
 
 const displayTasks = () => {
 
-
     let html = "";
 
-    allTasks.forEach(task => {
-        html +=
-        `
-        <div class="container-input">
-            <input ${task.status === "completed" ? "checked" : ""} onclick="updateTaskStatus(${task.id}, this)" type="checkbox" name="">
-            <input id=${task.id} type="text" onblur="editTask(${task.id}, this.value)" value="${task.value}" class="todoApp__input">
-            <i onclick=deleteTask(${task.id}) class="fa-regular fa-circle-xmark fa-2x xmark"></i>
-        </div>
-        `
+    const displaySortedTasks = (tasks) => {
+        if(tasks === "completed") tasks = allTasks.filter(task => task.status === "completed")
+        if(tasks === "pending") tasks = allTasks.filter(task => task.status === "pending")
+        if(tasks === "all") tasks = allTasks
+        tasks.forEach(task => {
+            html +=
+            `
+            <div class="container-input">
+                <input ${task.status === "completed" ? "checked" : ""} onclick="updateTaskStatus(${task.id}, this)" type="checkbox" name="">
+                <input id=${task.id} type="text" onblur="editTask(${task.id}, this.value)" value="${task.value}" class="todoApp__input ${task.status === "completed" ? "line-through" : ""}">
+                <i onclick=deleteTask(${task.id}) class="fa-regular fa-circle-xmark fa-2x xmark"></i>
+            </div>
+            `
+        })
+    
+        containerTasks.innerHTML = html
+    }
+    
+    sortsType.forEach(item => {
+        if(item.classList.contains("selected-sort")){
+            displaySortedTasks(item.id)
+        }
     })
-
-    containerTasks.innerHTML = html
 }
 
 
 // sehife yuklendiyi zaman display task funksiyasini ishkledir
-window.addEventListener("load", displayTasks);
+window.addEventListener("load", () => {
+    sortAll.classList.add("selected-sort")
+    displayTasks();
+})
 
 
 // yeni task elave edir allTask arrayina
@@ -109,6 +127,15 @@ const deleteTask = (id) => {
     displayTasks();
 }
 
+//deleteAll
+
+deleteAll.addEventListener("click", () => {
+    localStorage.removeItem("taskList");
+    allTasks = []
+    displayTasks()
+})
+
+
 // taski edit etmek ucun
 const editTask = (id, value) => {
     allTasks.forEach(task => {
@@ -119,31 +146,25 @@ const editTask = (id, value) => {
 }
 
 let sortAZ = false;
-
 // taski Elifba sirasi ile siralayir
 const sortAlphabet = () => {
     if(!sortAZ){
-        sortBtn.firstChild.classList = "fa-solid fa-arrow-down-short-wide fa-2x"
-        allTasks.sort((a,b) => {
-            if(a.value.toUpperCase() < b.value.toUpperCase()){
-                return -1
-            }
-            else if(b.value.toUpperCase() < a.value.toUpperCase()){
-                return 1
-            }
-            else return 0
-        })
+        sortBtn.firstChild.classList = "fa-solid fa-arrow-down-short-wide fa-2x";
+
+        allTasks.sort((a, b) => {
+            return a.value.localeCompare(b.value, undefined, {
+              numeric: true,
+              sensitivity: 'base'
+            });
+          });
         sortAZ = true;
     } else {
         sortBtn.firstChild.classList = "fa-solid fa-arrow-down-wide-short fa-2x"
         allTasks.sort((a,b) => {
-            if(a.value.toUpperCase() < b.value.toUpperCase()){
-                return 1
-            }
-            else if(b.value.toUpperCase() < a.value.toUpperCase()){
-                return -1
-            }
-            else return 0
+            return b.value.localeCompare(a.value, undefined, {
+                numeric: true,
+                sensitivity: 'base'
+              });
         })
         sortAZ = false;
     }
